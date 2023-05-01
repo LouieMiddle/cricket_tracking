1974f33bbf1deb1702571f4cf476d506b24d347a
LouieMiddleHawkeye <43111442+LouieMiddle@users.noreply.github.com>
2023-05-01 17:07:55 +0100

    more updates

--- examples/jos_buttler_seam_csv_example.py
+++ examples/jos_buttler_seam_csv_example.py
@@ -1,20 +1,36 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from plots.pitch_heatmap import pitch_heatmap
 
+
+def filter_by_pitch_x_pitch_y(data):
+    data = data[(data['pitchX'] >= -2) & (data['pitchX'] <= 2)]
+    data = data[(data['pitchY'] >= 0) & (data['pitchY'] <= 14)]
+    return data
+
+
 # Import Jos Buttler data
 df = pd.read_csv('../data/jos_buttler_seam_csv.csv')
 
-# Hawkeye Data has Y co-ord as meters from stumps towards bowler, so need to flip pitchX and stumpsX for plotting
+# Hawkeye Data has Y co-ord as meters from stumps towards bowler, so need to flip pitchX for plotting
 df['pitchX'] = -df['pitchX']
-df['stumpsX'] = -df['stumpsX']
+
+df = filter_by_pitch_x_pitch_y(df)
+seam = ['FAST_SEAM', 'MEDIUM_SEAM', 'SEAM']
+df = df[df['batter'] == 'Jos Buttler']
+df = df[df['bowlingStyle'].isin(seam)]
+df = df[df['rightArmedBowl'] == True]
 
 xy = np.array(df[['pitchX', 'pitchY']])
 runs = df.batterRuns
 
-title = 'Jos Buttler'
-subtitle_1 = 'Batting Strike Rate Zones IPL against seam'
+title = 'A Batter'
+subtitle_1 = 'All IPL deliveries against right arm seam for a single batter'
 subtitle_2 = 'From {0} balls faced with tracking enabled'.format(len(xy))
 legend_title = 'Strike Rate'
 
-pitch_heatmap(xy, runs, title, subtitle_1, subtitle_2, legend_title, min_balls=6, show_markers=True, measure='strike_rate')
+pitch_heatmap(xy, runs, title, subtitle_1, subtitle_2, legend_title, min_balls=6, show_markers=True,
+              measure='strike_rate')
+
+plt.show()
